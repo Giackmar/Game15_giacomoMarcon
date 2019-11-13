c0a2b9e6d3bbb41438b181fd396c33cc2608b3ac
Giackmar <giacomo.marcon@barsanti.edu.it>
2019-11-13 12:55:55 +0100

    Modifica131119gma
    inserisco commenti che descrivono i metodi che implementerò nel futuro

--- Gioco15_Marcon/ViewController.swift
+++ Gioco15_Marcon/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var Btn1: UIButton!
+    @IBOutlet weak var Btn2: UIButton!
+    @IBOutlet weak var Btn3: UIButton!
+    @IBOutlet weak var Btn4: UIButton!
+    @IBOutlet weak var Btn5: UIButton!
+    @IBOutlet weak var Btn6: UIButton!
+    @IBOutlet weak var Btn7: UIButton!
+    @IBOutlet weak var Btn8: UIButton!
+    @IBOutlet weak var Btn9: UIButton!
+    @IBOutlet weak var Btn10: UIButton!
+    @IBOutlet weak var Btn11: UIButton!
+    @IBOutlet weak var Btn12: UIButton!
+    @IBOutlet weak var Btn13: UIButton!
+    @IBOutlet weak var Btn14: UIButton!
+    @IBOutlet weak var Btn15: UIButton!
+    @IBOutlet weak var Btn16: UIButton!
+    var matrice: [[Int]] = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
+    var matriceButton: [[UIButton]] = [[]];
+    var quantitaMischio = 100;
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        matriceButton = [[Btn1,Btn2,Btn3,Btn4],[Btn5,Btn6,Btn7,Btn8],[Btn9,Btn10,Btn11,Btn12],[Btn13,Btn14,Btn15,Btn16]];
     }
+    //metodo che mi dice la posizione di un btn
+    //metodo che mi trova il 16(posizionevuota)
+    func ControlloSeSpostabile(Btn : UIButton) -> Bool
+    {
+        //trovo la posizione del bottone e controllo se posso spostarlo(se è adiacente alla posizione vuota(16))
+        return true//da modificare
+    }
+    
     @IBAction func Btn1(_ sender: UIButton) {
+        var possoSposarlo = ControlloSeSpostabile(Btn: Btn1)//se è possibile spostarlo lo faccio
+        if(possoSposarlo)
+        {
+            //faccio un metodo che mi dice la posizione del btn1 e la posizione di dove andrà
+        }
     }
     @IBAction func Btn2(_ sender: UIButton) {
     }
@@ -46,25 +80,9 @@ class ViewController: UIViewController {
     }
     @IBAction func Btn16(_ sender: UIButton) {
     }
-    //dichiaro
-    @IBOutlet weak var Btn1: UIButton!
-    @IBOutlet weak var Btn2: UIButton!
-    @IBOutlet weak var Btn3: UIButton!
-    @IBOutlet weak var Btn4: UIButton!
-    @IBOutlet weak var Btn5: UIButton!
-    @IBOutlet weak var Btn6: UIButton!
-    @IBOutlet weak var Btn7: UIButton!
-    @IBOutlet weak var Btn8: UIButton!
-    @IBOutlet weak var Btn9: UIButton!
-    @IBOutlet weak var Btn10: UIButton!
-    @IBOutlet weak var Btn11: UIButton!
-    @IBOutlet weak var Btn12: UIButton!
-    @IBOutlet weak var Btn13: UIButton!
-    @IBOutlet weak var Btn14: UIButton!
-    @IBOutlet weak var Btn15: UIButton!
-    @IBOutlet weak var Btn16: UIButton!
-    var matrice: [[Int]] = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
-    var quantitaMischio = 100;
+    
+    
+    
     func mischio()
     {
         for _ in 1...quantitaMischio
